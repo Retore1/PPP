a6a66c1a835709161a91ebb0742b3844826eff0e
Piero Puchuri <39770280+Retore1@users.noreply.github.com>
2018-07-05 17:35:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,25 @@ def mul(x,y):
 def poten(x,y):
   potencia = x^y
   return potencia
-def resta (x,y)
+def resta (x,y):
   resta=x-y
   return resta
-x=int(input())
-y=int(input())
+def div(x,y):
+    division=x/y
+    return division
+
+x=int(input("insertar numero :"))
+y=int(input("insertar numero:"))
+insertarsigno=input("insertarsigno:")
+if insertarsigno == "+":
+    sum(x,y)
+    print(sum(x,y))
+if insertarsigno == "-":
+    resta(x,y)
+    print(resta(x,y))
+if insertarsigno == "*":
+    mul(x,y)
+    print(sum(x,y))
+if insertarsigno == "/":
+    div(x,y)
+    print(div(x,y))
